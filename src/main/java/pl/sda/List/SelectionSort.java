f25c85a991fb2aee4eef525e8d92f0948482e3c0
Beata <bea.swirko@gmail.com>
2019-09-01 10:43:48 +0200

    list selection sort

--- src/main/java/pl/sda/List/SelectionSort.java
+++ src/main/java/pl/sda/List/SelectionSort.java
@@ -1,6 +1,30 @@
 package pl.sda.List;
 
 public class SelectionSort {
+
+    public IList sort(IList<Integer> listToSort){
+        int firstUnsortedIndex = 0;
+        do {
+            int minIndex = findMinValueIndex(listToSort, firstUnsortedIndex);
+            int tmp = listToSort.get(minIndex);
+            listToSort.set(minIndex, listToSort.get(firstUnsortedIndex));
+            listToSort.set(firstUnsortedIndex, tmp);
+        } while(++firstUnsortedIndex != listToSort.size());
+        return listToSort;
+    }
+
+    private int findMinValueIndex(IList<Integer> list, int startIndex) {
+        int minValue = list.get(startIndex);
+        int minValueIndex = startIndex;
+        for(int i = startIndex+1; i < list.size(); i++) {
+            if(list.get(i) < minValue) {
+                minValue = list.get(i);
+                minValueIndex = i;
+            }
+        }
+        return minValueIndex;
+    }
+
     public int[] sort(int[] tableToSort) {
         int firstUnsortedIndex = 0;
         do {
@@ -23,4 +47,9 @@ public class SelectionSort {
         }
         return minValueIndex;
     }
+
+
+
+
+
 }
